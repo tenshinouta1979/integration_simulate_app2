5c721f29c1cdbcad11b1d162b89a1b129ace2a28
ryanlee <ryan_lee@live.com>
2025-08-11 23:45:08 -0400

    initial commit

--- app2/obj/Debug/net8.0/app2.AssemblyInfo.cs
+++ app2/obj/Debug/net8.0/app2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("app2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e411334d27f1ddebcf666d163436a49c9a2725d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+323ecd4155e80891f86f07f81b6acda3d90e8bad")]
 [assembly: System.Reflection.AssemblyProductAttribute("app2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("app2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
